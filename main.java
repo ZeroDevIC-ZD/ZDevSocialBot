a6b89e1fab3aaf3f06c3d25627be8d2925c80cd7
GWarp <gwarp56@gmail.com>
2016-06-21 01:26:16 +0300

    dev started

---
+++ main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author AirCool
+ */
+public class main {
+    
+    String lang="EN";
+    
+    public static void main(String[] args) {
+        System.out.println("ChatBot by ZeroDev => AirTeam started \n Please choose your lang: \n 1)RU \n 2)EN");
+        Scanner scan=new Scanner(System.in);
+        if(scan.hasNextInt()){
+            
+        }
+    }
+}
