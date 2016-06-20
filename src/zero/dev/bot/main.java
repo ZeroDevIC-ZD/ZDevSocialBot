d90436d4f0e2dc78ed5c02a31c9dd2f248548483
AirCool <ZeroDev.AC@ya.ru>
2016-06-21 01:58:48 +0300

    real dev

---
+++ src/zero/dev/bot/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zero.dev.bot;
+
+import java.util.Scanner;
+/**
+ *
+ * @author AirCool
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("ChatBot by ZeroDev => AirTeam started \n Please choose your lang: \n 1)RU \n 2)EN");
+        Scanner scan=new Scanner(System.in);
+        if(scan.hasNextInt()){
+            
+        }
+    }
+    
+}
